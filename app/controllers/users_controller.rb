0874dcbc2813d0ca3041362016c61131527df3c8
jakapratama <jaka.pratama@wgshub.com>
2019-08-28 23:06:37 +0700

    benerin landing setelah input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Berhasil di tambahkan"
-      redirect_to @user
+      redirect_to action: "index"
     else
       flash[:error] =  @user.errors.full_messages.map{|k,v| k}.join("<br/>").html_safe
       render :new
